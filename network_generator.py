66f20bf4ed6fce9b7e88d90b45b48c74fb2e976c
Alireza <alireza.hashemi13@outlook.com>
2020-02-02 09:40:26 +0330

    Changed if to switch for optimization plus some minor changes

---
+++ network_generator.py
@@ -0,0 +1,37 @@
+import networkx as nx
+import numpy as np
+
+def network_generator(network_type, node, probability = None, neighbors = None):
+    '''
+    generate network from folowing network list
+    networks = 2d-lattice, erdos-renyi, watts-strogatz small-world, scale-free
+
+    ---- Parameters
+
+    network_type: type of network from list and started by 1. for example 1 refers to 2d-lattice
+
+    node: number of network nodes. for 2d-lattice it should be a squared integer
+
+    probability: Probability for edge creation. it use for erdos_renyi and watts-strogatz small-word networks
+
+    neighbors: The number of neighbors connected to each node in the watts-strogatz small-world network
+
+    ---- return
+
+    M: The adjacency matrix of the chosen network
+
+    '''
+    switch (network_type){
+        case 1:
+            G = nx.generators.lattice.grid_2d_graph(m=np.sqrt(node),n=n,periodic=True)
+        case 2:
+            G = nx.generators.random_graphs.erdos_renyi_graph(n=node,p=probability)
+        case 3:
+            G = nx.generators.random_graphs.watts_strogatz_graph(n=node,k=neighbors, p=probability)
+        case 4:
+            G = nx.generators.scale_free_graph(n=node)  
+    }
+
+    M = nx.to_numpy_array(G)
+
+    return M
