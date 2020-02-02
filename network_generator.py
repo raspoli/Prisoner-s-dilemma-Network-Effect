7b33e4c1cdf1dff18129a8c0eeb4d61d1891bd2a
Alireza <alireza.hashemi13@outlook.com>
2020-02-02 10:20:54 +0330

    Fixed bugs

--- network_generator.py
+++ network_generator.py
@@ -21,17 +21,17 @@ def network_generator(network_type, node, probability = None, neighbors = None):
     M: The adjacency matrix of the chosen network
 
     '''
-    switch (network_type){
-        case 1:
-            G = nx.generators.lattice.grid_2d_graph(m=np.sqrt(node),n=n,periodic=True)
-        case 2:
-            G = nx.generators.random_graphs.erdos_renyi_graph(n=node,p=probability)
-        case 3:
-            G = nx.generators.random_graphs.watts_strogatz_graph(n=node,k=neighbors, p=probability)
-        case 4:
-            G = nx.generators.scale_free_graph(n=node)  
-    }
-
+    if network_type==1:
+        G = nx.generators.lattice.grid_2d_graph(m=np.sqrt(node),n=n,periodic=True)
+    elif network_type==2:
+        G = nx.generators.random_graphs.erdos_renyi_graph(n=node,p=probability)
+    elif network_type==3:
+        G = nx.generators.random_graphs.watts_strogatz_graph(n=node,k=neighbors, p=probability)
+    elif network_type==4:
+        G = nx.generators.scale_free_graph(n=node)
+    else
+        G = nx.generators.random_graphs.erdos_renyi_graph(n=node,p=0.5)
+        
     M = nx.to_numpy_array(G)
 
     return M
