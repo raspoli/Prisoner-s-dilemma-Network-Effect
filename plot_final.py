60e6c37f42c96d44b12815c8711a0c175fa6b822
Alireza <alireza.hashemi13@outlook.com>
2020-02-04 12:44:44 +0330

    fixed edge mean

--- plot_final.py
+++ plot_final.py
@@ -4,9 +4,12 @@ import matplotlib.pyplot as plt
 def plot_final(L, max_seasons):
 
     x = [item for item in range(1, max_seasons + 1)]
-    for i in range(len(L)):
+    for i in range(int(len(L)/2)):
     # plt.plot(x,y1,x,y2,x,y3,x,y4,x,y5,x,y6,x,y7,x,y8,x,y9,x,y10,x,y11,x,y12,x,y13,x,y14,x,y15,x,y16)
         plt.plot(x,L[i,:])
+    for i in range(int(len(L)/2)):
+    # plt.plot(x,y1,x,y2,x,y3,x,y4,x,y5,x,y6,x,y7,x,y8,x,y9,x,y10,x,y11,x,y12,x,y13,x,y14,x,y15,x,y16)
+        plt.plot(x,L[int(len(L)/2)+i,:],linestyle='--')
     plt.xlabel('time')
     plt.ylabel('Distribution of strategies')
     plt.title('evolve of strategies')
@@ -15,4 +18,5 @@ def plot_final(L, max_seasons):
                      '0110','0111','1000','1001','1010','1100',
                      '1011','1101','1110','1111']
     plt.legend(listt, loc='upper right')
+    plt.ylim((0,1.1))
     plt.show()
